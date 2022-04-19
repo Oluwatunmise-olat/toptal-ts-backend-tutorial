25c4724fdd8f66716400ddbc905429505500136f
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-04-19 10:03:51 +0100

    add user routes

--- users/users.routes.ts
+++ users/users.routes.ts
@@ -7,6 +7,38 @@ export class UserRouteConfig extends CommonRouteConfig {
   }
 
   public configureRoutes(): express.Application {
-      return this.app
+    this.app
+      .route("/users")
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send("Users List");
+      })
+      .post((req: express.Request, res: express.Response) => {
+        res.status(201).send("Create Users Post");
+      });
+
+    this.app
+      .route("/users/:userId")
+      .all(
+        (
+          req: express.Request,
+          res: express.Response,
+          next: express.NextFunction
+        ) => {
+          return next();
+        }
+      )
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send("Get requested resource");
+      })
+      .put((req: express.Request, res: express.Response) => {
+        res.status(200).send("Put method for requested resource");
+      })
+      .patch((req: express.Request, res: express.Response) => {
+        res.status(200).send("Patch method for requested resource");
+      })
+      .delete((req: express.Request, res: express.Response) => {
+        res.status(200).send("Delete method for requested resource");
+      });
+    return this.app;
   }
 }
